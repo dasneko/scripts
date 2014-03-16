7e5094ca705352878f900551635232a3bc1b754f
Jusbol <julio.com@uol.com.br>
2014-03-15 23:57:14 -0300

    Update SimpleMalzahar.lua

--- SimpleMalzahar.lua
+++ SimpleMalzahar.lua
@@ -1,13 +1,16 @@
-local version = "0.1"
+local version = "0.2"
 
--- [[ Simple Malzahar by Jus v0.1 ]]
--- Orbwalk - Combo and harass
+-- [[ Simple Malzahar by Jus v0.1 VIP]]
+-- Orbwalk with auto-attack - Combo
 -- Combo and auto harass configurable
 -- Draw with lag free
--- VPredicition Q and W
+-- VPredicition Q and W - HitChance 2
 -- Minion last hit indicator
--- Mana alert
--- auto-update
+-- Mana alert (blue circle)
+-- Target draw
+-- Auto-update
+
+-- Encrypt here until the end. Encrypt here until the end. Encrypt here until the end.
 
 -----------------------------------------------------AUTOUPDATE
 
@@ -33,9 +36,9 @@ if autoupdateenabled then
 			end
 
 			if ServerVersion ~= nil and tonumber(ServerVersion) ~= nil and tonumber(ServerVersion) > tonumber(version) then
-				DownloadFile(UPDATE_URL.."?nocache"..myHero.charName..os.clock(), UPDATE_FILE_PATH, function () print("<font color=\"#FF0000\"> >> "..UPDATE_SCRIPT_NAME..": successfully updated. Reload (double F9) Please. ("..version.." => "..ServerVersion..")</font>") end)     
+				DownloadFile(UPDATE_URL.."?nocache"..myHero.charName..os.clock(), UPDATE_FILE_PATH, function () print("<font color=\"#FF0000\"><b>"..UPDATE_SCRIPT_NAME..":</b> successfully updated. Reload (double F9) Please. ("..version.." => "..ServerVersion..")</font>") end)     
 			elseif ServerVersion then
-				print("<font color=\"#FF0000\"> >> "..UPDATE_SCRIPT_NAME..": You have got the latest version: <b>"..ServerVersion.."</b></font>")
+				print("<font color=\"#FF0000\"><b>"..UPDATE_SCRIPT_NAME..":</b> You have got the latest version: <u><b>"..ServerVersion.."</b></u></font>")
 			end		
 			ServerData = nil
 		end
@@ -53,7 +56,6 @@ require "VPrediction"
 local VP = nil
 
 function OnLoad()
- --Update()
  Menu()
  Variaveis()
  OnDraw()
@@ -74,9 +76,8 @@ JMenu:addParam("ver", "Version", SCRIPT_PARAM_INFO, version)
 			JMenu.cfg:addParam("WCombo", "Use W Combo", SCRIPT_PARAM_ONOFF, true)	
 			JMenu.cfg:addParam("ECombo", "Use E Combo", SCRIPT_PARAM_ONOFF, true)
 			JMenu.cfg:addParam("RCombo", "Use R Commbo", SCRIPT_PARAM_ONOFF, true)
-		    --JMenu.cfg:addParam("UIgnite", "Use Ignite", SCRIPT_PARAM_ONOFF, false)
-			JMenu.cfg:addParam("CKey", "Combo Key", SCRIPT_PARAM_ONKEYDOWN, false, 32)
-			
+		    JMenu.cfg:addParam("UIgnite", "Use Ignite", SCRIPT_PARAM_ONOFF, false)
+			JMenu.cfg:addParam("CKey", "Combo Key", SCRIPT_PARAM_ONKEYDOWN, false, 32)			
 			
 		JMenu:addSubMenu("Auto Harass Settings", "harass")
 			JMenu.harass:addParam("Qharass", "Use Q Harass", SCRIPT_PARAM_ONOFF, false)
@@ -93,10 +94,14 @@ JMenu:addParam("ver", "Version", SCRIPT_PARAM_INFO, version)
 		JMenu:addSubMenu("Target Mode", "trgt")	
 		
 		JMenu:addSubMenu("Other", "upt")
-			JMenu.upt:addParam("up", "Auto Update", SCRIPT_PARAM_ONOFF, true)
-			JMenu.upt:addParam("manaa", "Show Mana", SCRIPT_PARAM_ONOFF, true)
+			JMenu.upt:addParam("up", "Auto Update", SCRIPT_PARAM_ONOFF, true)						
 			JMenu.upt:addParam("aa", "Auto Attack OrbWalk", SCRIPT_PARAM_ONOFF, true)
 			JMenu.upt:addParam("mini", "Minion Indicator", SCRIPT_PARAM_ONOFF, true)
+			JMenu.upt:addParam("aLvl", "Auto Level Skills", SCRIPT_PARAM_ONOFF, true)
+			JMenu.upt:addParam("dTgt", "Draw Target", SCRIPT_PARAM_ONOFF, false)
+			JMenu.upt:addParam("mn", "Mana Manager", SCRIPT_PARAM_SLICE, 10, 10, 100, 0)
+			JMenu.upt:addParam("trt", "Turrent Hits to Kill", SCRIPT_PARAM_ONOFF, true)
+			
 		
 	Alvo = TargetSelector(TARGET_LESS_CAST_PRIORITY, 900, DAMAGE_MAGIC)
 	JMenu.trgt:addTS(Alvo)
@@ -110,6 +115,7 @@ end
 -------------------------------------------------------DRAW
 
 function OnDraw()
+if not myHero.dead then
 
  if JMenu.paint.Qpaint then
   DrawCircle2(myHero.x, myHero.y, myHero.z, 900,  ARGB(255, 255, 000, 000))
@@ -123,25 +129,54 @@ function OnDraw()
  if JMenu.paint.Rpaint then
   DrawCircle2(myHero.x, myHero.y, myHero.z, 700,  ARGB(255, 255, 255, 000))
  end
- if JMenu.upt.manaa and false then
-  if myHero.mana < ComboManaCost(TheCombo) then
-   DrawNoMana()
-  end
- end 
-
+ if IsMyManaLow() then
+  DrawCircle2(myHero.x, myHero.y, myHero.z, 45, ARGB(255, 000, 000, 255))
+ end
+ 
+ if JMenu.upt.dTgt then
+ if Target ~= nil and not Target.dead then
+  for _, enemy in pairs(GetEnemyHeroes()) do
+        local pos= WorldToScreen(D3DXVECTOR3(enemy.x, enemy.y, enemy.z))
+        local posX = pos.x - 35
+        local posY = pos.y - 50
+        DrawText(">TARGET<",15,posX ,posY  ,ARGB(255,0,255,0))    
+    end 
+ end
+ end
+ 
  if JMenu.upt.mini then
   if enemyMinions ~= nil then
    for i, Minion in pairs(enemyMinions.objects) do
     if Minion ~= nil and not Minion.dead then
-    if ValidTarget(Minion, 1000) and Minion.health < getDmg("AD", Minion, myHero) then
-	 DrawCircle2(Minion.pos.x, Minion.pos.y, Minion.pos.z, 85, ARGB(255, 255, 255, 0))
+    if ValidTarget(Minion, 1000) and Minion.health < getDmg("AD", Minion, myHero) + 2 then
+	 DrawCircle2(Minion.pos.x, Minion.pos.y, Minion.pos.z, 85, ARGB(255, 255, 255, 000))
     end
 	end
    end
   end    
  end
+
+ if JMenu.upt.trt then
+  local turrets = GetTurrets();
+  local targetTurret = nil;
+  local hitsToTurret = 0;
+  for i, turret in pairs(turrets) do
+   if turret ~= nil and turret.team ~= myHero.team then
+    targetTurret = turret.object
+    hitsToTurret = round(turret.object.health / getDmg("AD", targetTurret, myHero))
+	 if hitsToTurret ~= 0 then
+	  local pos= WorldToScreen(D3DXVECTOR3(targetTurret.x, targetTurret.y, targetTurret.z))
+	  local posX = pos.x - 35
+      local posY = pos.y - 50
+        DrawText("Hits: "..hitsToTurret,15,posX ,posY  ,ARGB(255,0,255,0))  
+	 end
+	end
+ end
+ end
  
-end
+ end 
+end 
+
 
 function DrawCircle2(x, y, z, radius, color)
     local vPos1 = Vector(x, y, z)
@@ -165,10 +200,10 @@ function DrawCircleNextLvl(x, y, z, radius, width, color, chordlength)
     end
     DrawLines2(points, width or 1, color or 4294967295)
 end
+
 function round(num) 
  if num >= 0 then return math.floor(num+.5) else return math.ceil(num-.5) end
 end
-
 
 -------------------------------------------------------END DRAW
 
@@ -191,7 +226,6 @@ end
 end
 
 function OnProcessSpell(object,spell)
-
 if object == myHero then
  if spell.name:lower():find("attack") then
   lastAttack = GetTickCount() - GetLatency()/2
@@ -199,7 +233,6 @@ lastWindUpTime = spell.windUpTime*1000
 lastAttackCD = spell.animationTime*1000
   end
 end
-
 end
 
 function heroCanMove()
@@ -252,7 +285,7 @@ Wmana = {90, 95, 100, 105, 110}
 Emana = {60, 75, 90, 105, 120}
 Rmana = {150, 150, 150}
 TCombo = {_Q, _E, _W, _R}
-
+abilitySequence = {1,3,3,2,3,4,3,1,3,1,4,1,1,2,2,4,2,2}
 --if VIP_USER then
 -- _G.oldDrawCircle = rawget(_G, 'DrawCircle')
 -- _G.DrawCircle = DrawCircle2	
@@ -261,17 +294,37 @@ TCombo = {_Q, _E, _W, _R}
 end
 
 function Verificar()
-	qReady = (myHero:CanUseSpell(_Q) == READY) or (myHero:GetSpellData(_Q).currentCd < 1)
-    wReady = (myHero:CanUseSpell(_W) == READY) or (myHero:GetSpellData(_W).currentCd < 1)
-    eReady = (myHero:CanUseSpell(_E) == READY) or (myHero:GetSpellData(_E).currentCd < 1)
-    rReady = (myHero:CanUseSpell(_R) == READY) or (myHero:GetSpellData(_R).currentCd < 1) 
-		
+	qReady = (myHero:CanUseSpell(_Q) == READY) 
+    wReady = (myHero:CanUseSpell(_W) == READY) 
+    eReady = (myHero:CanUseSpell(_E) == READY) 
+    rReady = (myHero:CanUseSpell(_R) == READY)	
 	Alvo:update()
     Target = Alvo.target
 	autoupdateenabled = JMenu.upt.up
 	enemyMinions:update()
 end
 
+function IsMyManaLow()
+    if myHero.mana < (myHero.maxMana * ( JMenu.upt.mn / 100)) then
+        return true
+    else
+        return false
+    end
+end
+
+function levelSkills()
+ if myHero:GetSpellData(_Q).level + myHero:GetSpellData(_W).level + myHero:GetSpellData(_E).level + myHero:GetSpellData(_R).level < myHero.level then
+  local spellSlot = { SPELL_1, SPELL_2, SPELL_3, SPELL_4, }
+  local level = { 0, 0, 0, 0 }
+   for i = 1, myHero.level, 1 do
+    level[abilitySequence[i]] = level[abilitySequence[i]] + 1
+   end
+   for i, v in ipairs({ myHero:GetSpellData(_Q).level, myHero:GetSpellData(_W).level, myHero:GetSpellData(_E).level, myHero:GetSpellData(_R).level }) do
+    if v < level[i] then LevelSpell(spellSlot[i]) end
+   end
+ end
+end            
+              
 --------------------------------------------------------END MISC
 
 --------------------------------------------------------COMBOS
@@ -281,7 +334,7 @@ if not myHero.dead then
    if not isChanneling("Spell4") then
     OrbWalk(Target)
    end
- if Target ~= nil then
+ if Target ~= nil and not Target.dead then
   if not isChanneling("Spell4") then  
    if JMenu.cfg.QCombo and qReady then
     CastQ(Target) 
@@ -304,15 +357,15 @@ end
 
 function harrass()
 if not myHero.dead then
-  if Target ~= nil then  
+  if Target ~= nil and not Target.dead then  
    if not isChanneling("Spell4") then    
-	if JMenu.harass.Qharass and qReady and GetDistance(Target) < Qrange then
+	if JMenu.harass.Qharass and qReady and GetDistance(Target) <= Qrange then
 	 CastQ(Target)
 	end
-	if JMenu.harass.Wharras and wReady and GetDistance(Target) < Wrange then
+	if JMenu.harass.Wharras and wReady and GetDistance(Target) <= Wrange then
 	 CastW(Target)
 	end 
-	if JMenu.harass.Eharras and eReady and GetDistance(Target) < Erange then
+	if JMenu.harass.Eharras and eReady and GetDistance(Target) <= Erange then
      CastE(Target)
 	end   
    end    
@@ -320,33 +373,19 @@ if not myHero.dead then
 end
 end
 
-
---[[ FREE VERSION
 function CastQ(enemy)
-if GetDistance(enemy) <= Qrange then CastSpell(_Q, enemy.x, enemy.z) end
-end
-]]
-
-function CastQ(enemy)
-local CastPosition, HitChance, Postion = VP:GetLineCastPosition(enemy, 0.5, 100, Qrange, 1600, myHero, false)
- if HitChance >= 2 and GetDistance(CastPosition) < Qrange then
+local CastPosition, HitChance, Postion = VP:GetLineCastPosition(enemy, 0.4, 400, Qrange, 1600, myHero, false)
+ if HitChance >= 2 and GetDistance(CastPosition) <= Qrange then
   CastSpell(_Q, CastPosition.x, CastPosition.z)
  end 
 end
 
---[[ FREE VERSION
 function CastW(enemy)
-if GetDistance(enemy) <= Wrange then CastSpell(_W, enemy.x, enemy.z) end
-end
-]]
-
-function CastW(enemy)
-local CastPosition, HitChance, Postion = VP:GetCircularCastPosition(enemy, 0.5, 250, Wrange, 20)
- if HitChance >= 2 and GetDistance(CastPosition) < Wrange then
+local CastPosition, HitChance, Postion = VP:GetCircularCastPosition(enemy, 0.37, 250, Wrange, 20, myHero, false)
+ if HitChance >= 2 and GetDistance(CastPosition) <= Wrange then
   CastSpell(_W, CastPosition.x, CastPosition.z)
  end 
 end
-
 
 function CastE(enemy)
 if GetDistance(enemy) <= Erange then CastSpell(_E, enemy) end
@@ -361,6 +400,10 @@ end
 function OnTick()
 if JMenu.ligado then
 Verificar()
+
+ if JMenu.upt.aLvl then
+  levelSkills()
+ end
 
  if JMenu.cfg.CKey then
   Combo()
